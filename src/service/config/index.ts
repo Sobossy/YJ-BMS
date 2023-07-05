4f412644449554cb9d880c90f819e2d4cfc1637e
so_overbear <2579223304@qq.com>
2023-07-05 14:14:26 +0800

    更新了服务器ip

--- src/service/config/index.ts
+++ src/service/config/index.ts
@@ -3,10 +3,11 @@
 let BASE_URL = ''
 if (import.meta.env.DEV) {
   // development开发环境
-  BASE_URL = 'http://111.230.245.205:8880'
+  BASE_URL = 'http://121.37.196.89:8880'
+  // BASE_URL = 'http://codercba.com:8000'
 } else {
   // production生产环境
-  BASE_URL = 'http://111.230.245.205:8880'
+  BASE_URL = 'http://121.37.196.89:8880'
 }
 export const TIME_OUT = 10000
 export { BASE_URL }
